11e336e2c5aded4bc8d1d67c4b7ccf18887a60fc
Omayma Majid <omaymamjd05@gmail.com>
2021-07-17 23:56:10 +0100

    CRUD with Mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,62 @@
 const express = require('express');
-
+const bodyParser =  require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 
+const Thing = require('./models/Thing');
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://omayma:rootroot@cluster0.z5t7c.mongodb.net/Database?retryWrites=true&w=majority',
+{ useNewUrlParser: true,
+    useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+// CORS
 app.use((req, res, next) => {
-    console.log("test");
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-})
+//Body-Parser
+app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    res.json({message: "Response"});
-    next();
+//Middleware
+app.post('/api/stuff', (req, res, next) => {
+    delete req.body._id;
+    const thing = new Thing({
+      ...req.body
+    });
+    thing.save()
+          .then(res.status(201).json({ message: "Objet crée!!"}))
+          .catch(res.status(400).json({ error}));
 });
 
-app.use((req, res) => {
-    console.log("fin");
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({ _id: req.params.id})
+        .then(thing => res.status(200).json(thing))
+        .catch(error => res.status(404).json({ error}));
+});
+
+app.put('/api/stuff/:id', (req, res, next) => {
+   Thing.updateOne({ _id: req.params.id}, {...req.body, _id: req.params.id})
+        .then(() => res.status(200).json({message: "Objet modifié"}))
+        .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({_id: req.params.id})
+        .then(() => res.status(200).json({message: "Objet supprimé"}))
+        .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/stuff', (req, res, next) =>{
+  Thing.find()
+        .then(things => res.status(200).json(things))
+        .catch(error => res.status(400).json({ error}));
 });
 
 module.exports = app;
